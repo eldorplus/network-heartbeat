d09adbc585b2ec4ef108c2727af1c951a75b795b
Dustin Moorman <dustin.moorman@gmail.com>
2014-08-26 21:59:09 -0500

    adding config for issue #3

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+return array (
+	'administrator_email' => 'dustinmoorman@gmail.com',
+	'hosts' => array(),
+	'high_ping_threshold_ms' => '300',
+	'acceptable_packet_loss_percent' => '1',
+ 	'mandrill_creds' => array(),
+	'network_unavailable_wait_time_seconds' => 5 
+);
