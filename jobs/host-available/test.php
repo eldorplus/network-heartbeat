713c8b4dbb6c42c7730d8ef699f82e234cdfe0cf
Dustin Moorman <dustin.moorman@gmail.com>
2014-08-28 17:34:15 -0500

    dropping new lines

--- jobs/host-available/test.php
+++ jobs/host-available/test.php
@@ -20,7 +20,7 @@ class Test extends \Network_HeartBeat\Jobs\Base
 		$ms = array();
 		foreach ($r as $l){
 			 if(strpos($l,$s) !== FALSE)
-             $ms[] = (int) substr($l,strrpos($l, $s)+strlen($s)) . "\r\n";
+             $ms[] = (int) substr($l,strrpos($l, $s)+strlen($s));
 		}
 
 	}
