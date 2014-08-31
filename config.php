e24097d9b5f7f630ef902086c035fbcb920faebf
Dustin Moorman <dustin.moorman@gmail.com>
2014-08-30 22:11:03 -0500

    moving packet loss to host available

--- config.php
+++ config.php
@@ -3,11 +3,11 @@ return array (
 	'administrator_email' => 'dustinmoorman@gmail.com',
 	'hosts' => array(),
 	'high_ping_threshold_ms' => '300',
-	'acceptable_packet_loss_percent' => '1',
  	'mandrill_creds' => array(),
 	'jobs' => array(
 		'hostAvailable' => array(
 			'ping_count' = 3,
+			'acceptable_packet_loss_percent' => 1,
 		),
 		'networkAvailable' => array(
 			'wait_time_seconds' => 5,
