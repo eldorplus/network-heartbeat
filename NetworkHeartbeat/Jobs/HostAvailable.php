3e27250c400167cd4af0414af8d4484b743763b8
Dustin Moorman <dustin.moorman@gmail.com>
2014-08-31 17:07:55 -0500

    moving it here

---
+++ NetworkHeartbeat/Jobs/HostAvailable.php
@@ -0,0 +1,33 @@
+<?php 
+
+namespace NetworkHeartbeat\Jobs\HostAvailable;
+
+class HostAvailable extends \NetworkHeartBeat\Jobs\Base 
+{
+
+	protected $_ping_count;
+
+	public function __construct()
+	{
+		$this->setWithConfig();
+	}
+
+	public function execute($host)
+	{
+		$token = 'time=';
+		exec("ping  -c {$this->_ping_count} {$host}", $result);
+
+		$ms = array();
+		foreach ($result as $line){
+			if(strpos($line,$token) !== FALSE){
+				$ms[] = (int) substr($line,strrpos($line, $token)+strlen($token));
+			}
+		}
+
+	}
+
+	public function setWithConfig()
+	{
+		$this->_ping_count = $this->getConfig()->ping_count;
+	}
+}
