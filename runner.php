29922bb02f1fb2214b2fa8405cde1297cf5ac2e6
Dustin Moorman <dustin.moorman@gmail.com>
2014-08-31 16:54:44 -0500

    adding autoload register

--- runner.php
+++ runner.php
@@ -3,3 +3,9 @@
 define('APPLICATION_PATH', dirname(__FILE__));
 define('JOB_DIRECTORY', APPLICATION_PATH . '/NetworkHeartbeat/Jobs/');
 
+spl_autoload_register(function($class){
+    require_once(str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php');
+});
+
+
+
