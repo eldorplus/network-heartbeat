8d6f883aa329a5112ca4353aa89b6e9025f2bc1e
Dustin Moorman <dustin.moorman@gmail.com>
2014-08-31 17:49:14 -0500

    putting placeholders

--- config.php
+++ config.php
@@ -2,13 +2,17 @@
 return array (
 	'administrator_email' => 'dustinmoorman@gmail.com',
 	'hosts' => array(),
- 	'mandrill_creds' => array(),
+ 	'mandrill_creds' => array(
+ 		'apiKey' => 'b'
+	),
 	'jobs' => array(
 		'hostAvailable' => array(
 			'ping_count' => 3,
 			'high_ping_threshold_ms' => 300,
 			'acceptable_packet_loss_percent' => 1,
-			'hosts' => array(),
+			'hosts' => array(
+				'test' => '192.168.1.1'
+			),
 		),
 		'networkAvailable' => array(
 			'wait_time_seconds' => 5,
