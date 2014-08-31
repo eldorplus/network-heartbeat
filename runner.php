ea2057457f580d2f9ac5902df644f64bba0cd70f
Dustin Moorman <dustin.moorman@gmail.com>
2014-08-31 17:33:15 -0500

    creating runner

--- runner.php
+++ runner.php
@@ -7,5 +7,30 @@ spl_autoload_register(function($class){
     require_once(str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php');
 });
 
+// runner will take a job name from argv, (class name)
 
+$Job = factory($argv[1]);
 
+print_r($Job);
+
+// runner should find job
+
+// runner should instantiate job
+
+// runner should set job configs
+
+// runner should execute job
+
+// runner should supply communication to mailer of errors
+
+function factory($job_name){
+    try {
+        if(file_exists(JOB_DIRECTORY . $job_name . '.php')){
+            $job = 'NetworkHeartbeat\Jobs\\' . $job_name;
+            return new $job;
+        }        
+        throw new Exception('Job does not exist.');
+    } catch (Exception $e){
+        return $e->getMessage();
+    }
+}
