7052bc448fd1e481eeafbf8d44e0cb52010264b2
Dustin Moorman <dustin.moorman@gmail.com>
2014-09-01 11:05:40 -0500

    refactoring namesapce

---
+++ NetworkHeartbeat/Job/Base.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace NetworkHeartbeat\Jobs;
+
+abstract class Base 
+{
+	private $_config;
+
+	abstract function setWithConfig();
+
+	protected function getConfig()
+	{
+		return $this->_config;
+	}
+
+	public function setConfig(\stdClass $config){
+		$this->_config = $config;
+	}
+}
