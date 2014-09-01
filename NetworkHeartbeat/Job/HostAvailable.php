5c86efd62115900424d5951c673a62a8fe0072b2
Dustin Moorman <dustin.moorman@gmail.com>
2014-09-01 13:32:15 -0500

    building out the job

--- NetworkHeartbeat/Job/HostAvailable.php
+++ NetworkHeartbeat/Job/HostAvailable.php
@@ -4,17 +4,40 @@ namespace NetworkHeartbeat\Job;
 
 class HostAvailable extends \NetworkHeartBeat\Job\Base 
 {
-	public function execute($host)
+	public function execute()
 	{
-		$token = 'time=';
-		exec("ping  -c {$this->_ping_count} {$host}", $result);
+		try {
+			if(sizeof($this->getConfig()->hosts) > 0){
+				$token = 'time=';
+				foreach($this->getConfig()->hosts as $host){
 
-		$ms = array();
-		foreach ($result as $line){
-			if(strpos($line,$token) !== FALSE){
-				$ms[] = (int) substr($line,strrpos($line, $token)+strlen($token));
-			}
+					exec("ping  -c {$this->getConfig()->ping_count} {$host}", $result);
+					
+					$ms = array();
+					foreach ($result as $line){
+						echo "\r\nLine: $line";
+						if(strpos($line,$token) !== false){
+							$ms[] = (int) substr($line,strrpos($line, $token) + strlen($token));
+						}
+
+						if(strrpos($line, '%')){
+							$loss = (int) substr($line, strrpos($line, ',')); 
+						}
+					}
+
+					foreach($ms as $msec){
+						if($msec > $this->getConfig()->high_ping_threshold_ms){
+							$this->triggerEvent(\NetworkHeartbeat\Event::HIGH_PING);
+						}
+					}
+				}	
+			} else {
+				throw new Exception('No hosts set to test in configuration.');
+			}	
+		} catch (Exception $e){
+			$this->triggerEvent(\NetworkHeartbeat\Event::JOB_EXCEPTION, $e->getMessage());
+			return false;
 		}
-
+		return true;
 	}
 }
