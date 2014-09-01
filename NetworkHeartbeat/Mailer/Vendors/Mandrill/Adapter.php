7a6e15e44ed418b53ac5330fee4b7d22fdc1de1d
Dustin Moorman <dustin.moorman@gmail.com>
2014-08-31 20:57:00 -0500

    namespace fix

--- NetworkHeartbeat/Mailer/Vendors/Mandrill/Adapter.php
+++ NetworkHeartbeat/Mailer/Vendors/Mandrill/Adapter.php
@@ -4,7 +4,7 @@ namespace \NetworkHeartbeat\Mailer\Vendor\Mandrill;
 
 class Adapter
 {
-    public function __construct(stdClass $Config){
+    public function __construct(\stdClass $Config){
         switch($Config->mailer->vendor->implementation){
             default:
             case 'Dustinmoorman':
