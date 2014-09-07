3749f1d6935e2dfd50939c0da7281991c8a3428c
Dustin Moorman <dustin.moorman@gmail.com>
2014-09-07 19:38:23 +0000

    adding some text to startup

--- eventWatch.php
+++ eventWatch.php
@@ -2,11 +2,17 @@
 
 require 'vendor/autoload.php';
 
+echo ":: EventWatch - Network Heartbeat";
+
 $server = React\EventLoop\Factory::create();
 $context = new React\ZMQ\Context($server);
 
+echo "\r\nServer created...";
+
 $listen = $context->getSocket(ZMQ::SOCKET_PULL);
 $listen->bind('tcp://127.0.0.1:5555');
+
+echo "Bound to socket, port 5555.";
 
 $listen->on('message', function ($msg) {
 	echo "Rec: " . print_r($msg,1);
@@ -16,4 +22,5 @@ $listen->on('error', function ($e) {
 	echo "ERR: " . $e->getMessage();
 });
 
+echo "\r\nWatching for events.\r\n";
 $server->run();
