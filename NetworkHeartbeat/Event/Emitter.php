b809d51813daa0d7d110886b819ba750e9f2984d
Dustin Moorman <dustin.moorman@gmail.com>
2014-09-07 19:58:40 +0000

    adding event emitter

---
+++ NetworkHeartbeat/Event/Emitter.php
@@ -0,0 +1,27 @@
+<?php
+require 'vendor/autoload.php';
+
+namespace NetworkHeartbeat\Event;
+
+class Emitter 
+{
+
+	protected $loop;
+
+	protected $context;
+
+	public function __construct()
+	{
+		$this->loop = \React\EventLoop\Factory::create();
+		$this->context = new \React\ZMQ\Context($this->loop);
+	}
+
+	public function transmit($payload)
+	{
+		$push = $this->context->getSocket(\ZMQ::SOCKET_PUSH);
+		$push->connect('tcp://127.0.0.1:5555');
+		$push->send(serialize($payload));
+		$this->loop->run();
+	}
+
+}
