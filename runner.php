109fd74bb39c40f30db027fba0f0c30b1fbc814b
Dustin Moorman <dustin.moorman@gmail.com>
2014-09-08 01:14:02 +0000

    changes to runner

--- runner.php
+++ runner.php
@@ -1,11 +1,6 @@
 <?php
 
-define('APPLICATION_PATH', dirname(__FILE__));
-define('JOB_DIRECTORY', APPLICATION_PATH . '/NetworkHeartbeat/Job/');
-
-spl_autoload_register(function($class){
-    require_once(str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php');
-});
+require_once 'bootstrap.php';
 
 $Job = factory($argv[1]);
 
@@ -13,11 +8,6 @@ if(!$Job instanceof \NetworkHeartbeat\Job\Base){
     echo $Job;
     exit;
 }
-
-$config_data = include 'config.php';
-$Config = new stdClass();
-
-recursivelyHydrate($Config, $config_data);
 
 if(isset($Config->job->$argv[1])){
     $Job->setConfig($Config->job->$argv[1]);
@@ -30,32 +20,5 @@ if($Config->mailer->on){
 
 $result = $Job->execute();
 
-// $Mailer->setBody('<h1>Network Heartbeat Test Victory!!!!</h1>');
-// $Mailer->addRecipient($Config->administrator->email, $Config->administrator->name);
-// $result = $Mailer->send();
-
 print_r($result);
 
-
-function factory($job_name){
-    try {
-        if(file_exists(JOB_DIRECTORY . $job_name . '.php')){
-            $job = 'NetworkHeartbeat\Job\\' . $job_name;
-            return new $job;
-        }        
-        throw new Exception('Job does not exist.');
-    } catch (Exception $e){
-        return $e->getMessage();
-    }
-}
-
-function recursivelyHydrate($object, $array){
-    foreach($array as $k => $v){
-        if(is_array($v)){
-            $object->$k = new stdClass();
-            recursivelyHydrate($object->$k, $v);
-        } else {
-            $object->$k = $v;
-        }
-    }
-}
