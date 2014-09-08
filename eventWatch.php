01fc95910d21d15944b1f1213f747466b3b0bd08
Dustin Moorman <dustin.moorman@gmail.com>
2014-09-08 01:14:15 +0000

    changes to event watch

--- eventWatch.php
+++ eventWatch.php
@@ -1,13 +1,15 @@
 <?php
 
-require 'vendor/autoload.php';
+require 'bootstrap.php';
 
 echo ":: EventWatch - Network Heartbeat";
 
 $server = React\EventLoop\Factory::create();
 $context = new React\ZMQ\Context($server);
+$mailer = \NetworkHeartbeat\Mailer\Strategy::getMailer($Config);
 
 echo "\r\nServer created...";
+
 
 $listen = $context->getSocket(ZMQ::SOCKET_PULL);
 $listen->bind('tcp://127.0.0.1:5555');
@@ -15,7 +17,14 @@ $listen->bind('tcp://127.0.0.1:5555');
 echo "Bound to socket, port 5555.";
 
 $listen->on('message', function ($msg) {
-	echo "\r\nRec: " . print_r(unserialize($msg),1);
+	$event = unserialize($msg);
+
+	$mailer->setBody($event->message);
+	$mailer->setTitle($event->event);
+	$mailer->addRecipient($Config->administrator->email, $Config->administrator->name);
+
+	echo "\r\nEvent Triggered " . $event->event . ' with message: ' . $event->message;
+	print_r($mailer->send()); 
 });
 
 $listen->on('error', function ($e) {
