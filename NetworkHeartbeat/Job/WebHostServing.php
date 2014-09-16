26912d4f92bc03a61876793b0c0e4186f69a1538
Dustin Moorman <dustin.moorman@gmail.com>
2014-09-16 12:15:47 -0500

    base job meat

--- NetworkHeartbeat/Job/WebHostServing.php
+++ NetworkHeartbeat/Job/WebHostServing.php
@@ -8,37 +8,13 @@ class HostAvailable extends \NetworkHeartbeat\Job\Base
 	{
 		try {
 			if(sizeof($this->getConfig()->hosts) > 0){
-				$token = 'time=';
-				foreach($this->getConfig()->hosts as $host){
-
-					exec("ping  -c {$this->getConfig()->ping_count} {$host}", $result);
-					
-					$ms = array();
-					foreach ($result as $line){
-						if(strpos($line,$token) !== false){
-							$ms[] = (int) substr($line,strrpos($line, $token) + strlen($token));
-						}
-
-						if(strrpos($line, '%')){
-							$loss = (int) substr($line, strrpos($line, ',')); 
-						}
-					}
-
-					if($loss == 100) {
-						$this->triggerEvent(\NetworkHeartbeat\Event\Event::HOST_UNAVAILABLE, $host);
-					} else {
-
-						if ($loss > $this->getConfig()->acceptable_packet_loss_percent) {
-							$this->triggerEvent(\NetworkHeartbeat\Event\Event::PACKET_LOSS_THRESHOLD, $host);
-						}
-					
-						foreach($ms as $msec){
-							if($msec > $this->getConfig()->high_ping_threshold_ms){
-								$this->triggerEvent(\NetworkHeartbeat\Event\Event::HOST_HIGH_PING, $host);
-							}
-						}
-					}
-				}	
+				foreach ($this->getConfig()->hosts as $host => $url) {	
+					$http = \curl_init($url);
+					$result = \curl_exec($http);
+					$http_status = \curl_getinfo($http, CURLINFO_HTTP_CODE);
+					\curl_close($http);
+					echo $host ' registering ' . $http_status;
+				}
 			} else {
 				throw new Exception('No hosts set to test in configuration.');
 			}	
