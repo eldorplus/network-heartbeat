53cf0cab28ff80d86af63ffb065ccacaa1f331c6
Dustin Moorman <dustin.moorman@gmail.com>
2014-09-17 08:14:45 -0500

    Update WebHostServing.php

--- NetworkHeartbeat/Job/WebHostServing.php
+++ NetworkHeartbeat/Job/WebHostServing.php
@@ -2,7 +2,7 @@
 
 namespace NetworkHeartbeat\Job;
 
-class HostAvailable extends \NetworkHeartbeat\Job\Base 
+class WebHostServing extends \NetworkHeartbeat\Job\Base 
 {
 	public function execute()
 	{
